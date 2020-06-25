a2c7f842eeab5473b4c1c0a135af43067ebaab33
Daniel Rotband <rotbandnunes@hotmail.com>
2020-06-25 04:26:40 -0300

    Commit da primeira aplicação django. Exercício proposto

--- core/views.py
+++ core/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-def hello(request, nome, idade):
-	return HttpResponse('<h1>Hello {} de {} anos</h1>'.format(nome, idade))
+def hello(request, nome):
+	return HttpResponse('<h1>Hello {}</h1>'.format(nome))
+
+def soma(request, num1, num2):
+	soma = num1 + num2
+	return HttpResponse('<h1>soma de {} + {} = {} </h1>'.format(num1, num2, soma))
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -20,4 +20,5 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/<nome>/<int:idade>/', views.hello),
+    path('soma/<int:num1>/<int:num2>/', views.soma),
 ]
